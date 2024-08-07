f292f691e032c7ad260fa508fd30eb523d413ace
Tamtemtom020604 <timo.widyanvolta-2021@ftmm.unair.ac.id>
2024-08-07 18:22:21 +0700

    Add ChakraUI and react-router-dom setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home/Home"
 
 function App() {
 
   return (
     <>
-      hi
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+import App from "./App.jsx";
+import "./index.css";
+
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode("gray.100", "#000")(props),
+      color: mode("gray.800", "whiteAlpha.900")(props),
+    },
+  }),
+};
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: "false",
+};
+const theme = extendTheme({ config, styles });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router><ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider></Router>
+    
+  </React.StrictMode>
+);
